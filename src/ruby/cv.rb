7a8446e84b143361b9e4344379db75808b2f62f1
David O Neill <dmz.oneill@gmail.com>
2023-02-04 08:25:07 +0000

    Ruby lint

--- src/ruby/cv.rb
+++ src/ruby/cv.rb
@@ -1,149 +1,151 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require "yaml"
+require 'yaml'
 
-$cv_details = ENV["cv_file"]
-$template = ENV["template_file"]
-$out_html = ENV["output_file"]
-
-$template_html = File.read($template)
-$cv_yaml = YAML.load(File.read($cv_details))
-
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def section_replace_scalars(section, replacements)
-  result = ""
-
-  if replacements.instance_of?(String) and section.include? "{list_item}"
-    return section.gsub("{list_item}", replacements + "\n")
-  elsif section.include? "{list_item}"
-    details = ""
-    replacements.each { |x|
+  if replacements.instance_of?(String) && section.include?('{list_item}')
+    section.gsub('{list_item}', "#{replacements}\n")
+  elsif section.include? '{list_item}'
+    details = ''
+    replacements.each do |x|
       copy = section
-      copy = copy.gsub("{list_item}", x.strip + "\n")
+      copy = copy.gsub('{list_item}', "#{x.strip}\n")
       details += copy
-    }
-    return details
+    end
+    details
   else
     matches = section.scan(/\{(.*?)\}/i)
-    matches.each { |x|
-      section = section.gsub("{" + x[0] + "}", replacements[x[0]].strip + "\n")
-    }
-    return section
+    matches.each do |x|
+      section = section.gsub("{#{x[0]}}", "#{replacements[x[0]].strip}\n")
+    end
+    section
   end
-  return result
 end
 
-def references
-  match = $template_html.scan(/<references>(.*?)<\/references>/ms)
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
-  replacement = ""
-  $cv_yaml["references"].each { |x|
+def references(template_html, cv_yaml)
+  match = template_html.scan(%r{<references>(.*?)</references>}ms)
+
+  replacement = ''
+  cv_yaml['references'].each do |x|
     replacement += section_replace_scalars(match[0][0], x)
-  }
+  end
 
-  $template_html = $template_html.gsub(/<references>(.*?)<\/references>/ms, replacement)
+  template_html.gsub(%r{<references>(.*?)</references>}ms, replacement)
 end
 
-def pages
-  for count in 0..$cv_yaml["pages"].length - 1
+# rubocop:disable Metrics/MethodLength, Metrics/BlockLength, Metrics/AbcSize
+def pages(template_html, cv_yaml)
+  (0..cv_yaml['pages'].length - 1).each do |count|
     x = (count + 1).to_s
-    match = $template_html.scan(/<page#{x}>(.*?)<\/page#{x}>/ms)
+    match = template_html.scan(%r{<page#{x}>(.*?)</page#{x}>}ms)
 
-    page = $cv_yaml["pages"][count]
-    jobs = page["jobs"]
+    page = cv_yaml['pages'][count]
+    jobs = page['jobs']
 
-    replacement = ""
-    jobs.each { |job|
+    replacement = ''
+    jobs.each do |job|
       job_section = match[0][0]
-      role_match = job_section.scan(/<roles>(.*?)<\/roles>/ms)
+      role_match = job_section.scan(%r{<roles>(.*?)</roles>}ms)
 
-      roles_replacement = ""
-      job["roles"].each { |role|
+      roles_replacement = ''
+      job['roles'].each do |role|
         role_section = role_match[0][0]
-        details_match = job_section.scan(/<details>(.*?)<\/details>/m)
+        details_match = job_section.scan(%r{<details>(.*?)</details>}m)
 
-        details_replacement = ""
-        role["details"].each { |detail|
+        details_replacement = ''
+        role['details'].each do |detail|
           detail_copy = section_replace_scalars(details_match[0][0], detail)
           details_replacement += detail_copy
-        }
-        role_section = role_section.gsub(/<details>(.*?)<\/details>/ms, details_replacement)
+        end
+        role_section = role_section.gsub(%r{<details>(.*?)</details>}ms, details_replacement)
         role_section = section_replace_scalars(role_section, role)
         roles_replacement += role_section
-      }
+      end
 
-      job_section = job_section.gsub(/<roles>(.*?)<\/roles>/ms, roles_replacement)
+      job_section = job_section.gsub(%r{<roles>(.*?)</roles>}ms, roles_replacement)
       job_section = section_replace_scalars(job_section, job)
       replacement += job_section
-    }
-
-    $template_html = $template_html.gsub(/<page#{x}>(.*?)<\/page#{x}>/ms, replacement)
-  end
-end
-
-def roles
-  $template_html = $template_html.gsub(/{current-role}/ms, $cv_yaml["roles"][0])
-
-  match = $template_html.scan(/<previous-roles>(.*?)<\/previous-roles>/ms)
-  rows = ""
-  for count in 1..$cv_yaml["roles"].length - 1
-    if count == 0
-      next
     end
 
+    template_html = template_html.gsub(%r{<page#{x}>(.*?)</page#{x}>}ms, replacement)
+  end
+  template_html
+end
+
+# rubocop:enable Metrics/MethodLength, Metrics/BlockLength, Metrics/AbcSize
+
+def roles(template_html, cv_yaml)
+  template_html = template_html.gsub(/{current-role}/ms, cv_yaml['roles'][0])
+
+  match = template_html.scan(%r{<previous-roles>(.*?)</previous-roles>}ms)
+  rows = ''
+  (1..cv_yaml['roles'].length - 1).each do |count|
+    next if count.zero?
+
     row = match[0][0]
-    row = section_replace_scalars(row, $cv_yaml["roles"][count])
+    row = section_replace_scalars(row, cv_yaml['roles'][count])
     rows += row
   end
 
-  $template_html = $template_html.gsub(/<previous-roles>(.*?)<\/previous-roles>/ms, rows)
+  template_html.gsub(%r{<previous-roles>(.*?)</previous-roles>}ms, rows)
 end
 
-def replace
-  references()
-  pages()
-  roles()
-
-  $cv_yaml.each { |x, value|
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength, Metrics/BlockLength, Metrics/AbcSize
+def replace_one(template_html, cv_yaml)
+  cv_yaml.each do |x, value|
     if value.instance_of?(String)
-      replacement = value
-      if replacement.include? "\\n"
-        replacement = replacement.gsub("\\n", "<br/>")
-      end
-      $template_html = $template_html.gsub("{" + x + "}", value)
+      replacement = value.gsub('\\n', '<br/>') if value.include? '\\n'
+      template_html = template_html.gsub("{#{x}}", replacement)
     else
-      match_instance = $template_html.scan(/<#{x}>(.*?)<\/#{x}>/ms)
+      match_instance = template_html.scan(%r{<#{x}>(.*?)</#{x}>}ms)
 
-      if match_instance.instance_of?(Array) == false
-        next
-      end
+      next if match_instance.instance_of?(Array) == false
 
-      if match_instance.length == 0
-        next
-      end
+      next if match_instance.empty?
 
-      list_replacement = ""
-      $cv_yaml[x].each { |y|
+      list_replacement = ''
+      cv_yaml[x].each do |y|
         copy = match_instance[0][0]
         if y.instance_of?(String) == false
-          if y.include? "list"
-            match = copy.scan(/<children>(.*?)<\/children>/ms)
-            sublist = section_replace_scalars(match[0][0], y["list"])
-            copy = copy.gsub(/<children>(.*?)<\/children>/ms, sublist)
-            copy = section_replace_scalars(copy, y["item"])
+          if y.include? 'list'
+            match = copy.scan(%r{<children>(.*?)</children>}ms)
+            sublist = section_replace_scalars(match[0][0], y['list'])
+            copy = copy.gsub(%r{<children>(.*?)</children>}ms, sublist)
+            copy = section_replace_scalars(copy, y['item'])
           else
-            copy = copy.gsub(/{list_item}.*?<ul.*?<\/ul>/ms, y["item"])
+            copy = copy.gsub(%r{{list_item}.*?<ul.*?</ul>}ms, y['item'])
           end
           list_replacement += copy
         elsif y.instance_of?(String)
           copy = section_replace_scalars(copy, y)
           list_replacement += copy
         end
-      }
+      end
 
-      $template_html = $template_html.gsub(/<#{x}>(.*?)<\/#{x}>/ms, list_replacement)
+      template_html = template_html.gsub(%r{<#{x}>(.*?)</#{x}>}ms, list_replacement)
     end
-  }
+  end
 end
 
-replace()
-File.write($out_html, $template_html)
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength, Metrics/BlockLength, Metrics/AbcSize
+
+def replace
+  cv_details = ENV['cv_file']
+  template = ENV['template_file']
+  out_html = ENV['output_file']
+
+  template_html = File.read(template)
+  cv_yaml = YAML.safe_load(File.read(cv_details))
+
+  template_html = references(template_html, cv_yaml)
+  template_html = pages(template_html, cv_yaml)
+  template_html = roles(template_html, cv_yaml)
+
+  File.write(out_html, template_html)
+end
+
+replace
