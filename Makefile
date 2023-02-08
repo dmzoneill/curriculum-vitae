d104763dec2928e36eb9a769dc84a20bdba05aae
David O Neill <dmz.oneill@gmail.com>
2023-02-08 19:17:00 +0000

    csharp

--- Makefile
+++ Makefile
@@ -7,10 +7,45 @@ CWD := $(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
 version := $(shell grep 'version=.*' version | awk -F'=' '{print $$2}')
 next := $(shell echo ${version} | awk -F. '/[0-9]+\./{$$NF++;print}' OFS=.)
 jar_files := "jackson-annotations-2.14.2.jar:jackson-core-2.14.1.jar:jackson-databind-2.14.1.jar:jackson-dataformat-yaml-2.14.2.jar"
+nonlint_target := $(shell echo $(MAKECMDGOALS) | awk -F'-' '{print $$1}')
 
 export cv_file := $(CWD)/input/cv.yaml
 export template_file := $(CWD)/input/template.html
 export output_file := $(CWD)/out/$(MAKECMDGOALS).html
+
+markup-lint:
+	{ \
+		cd css; \
+		curl -o .stylelintrc.json https://raw.githubusercontent.com/dmzoneill/dmzoneill/main/.github/linters/.stylelintrc.json; \
+		npm install --save-dev stylelint stylelint-config-standard; \
+		npx stylelint --fix *.css; \
+		rm .stylelintrc.json; \n
+	}
+
+	{ \
+		cd input; \
+		npm install --save-dev htmlhint; \
+		npx htmlhint *.html; \
+	}
+
+	{ \
+		cd input; \
+		npm install --save-dev yamllint; \
+		npx yamllint *.yaml; \
+	}
+
+csharp:
+	{ \
+		cd $(CWD)/src/$@/; \
+		mdtool build cv.csproj; \
+		mono --debug ./bin/Debug/cv.exe; \
+	}
+
+go-lint:
+	{ \
+		cd $(CWD)/src/${nonlint_target}/; \
+		golangci-lint run --fix cv.go; \
+	}
 
 go:
 	{ \
@@ -20,6 +55,12 @@ go:
 		go get gopkg.in/yaml.v2; \
 		go get github.com/gookit/goutil/dump; \
 		go run cv.go; \
+	}
+
+cpp-lint:
+	{ \
+		cd $(CWD)/src/${nonlint_target}/; \
+		clang-format -i cv.cpp;\ 
 	}
 
 cpp:
@@ -37,20 +78,65 @@ java:
 		java -cp ".:${jar_files}" cv; \
 	}
 
-perl:
-	perl "$(CWD)/src/$@/cv.pl" 
+perl:	
+	{ \
+		cd $(CWD)/src/$@/; \
+		perl cv.pl; \
+	}
+
+javascript-lint:
+	{ \
+		cd $(CWD)/src/${nonlint_target}/; \
+		npm install standard --save-dev
+		npx standard --fix; \
+	}
+	
 
 javascript:
-	node "$(CWD)/src/$@/cv.js" 
+	{ \
+		cd $(CWD)/src/$@/; \
+		node cv.js; \
+	}
+
+ruby-lint:
+	{ \
+		cd $(CWD)/src/${nonlint_target}/; \
+		ruby cv.rb; \
+	}
 
 ruby:
-	ruby "$(CWD)/src/$@/cv.rb" 
+	{ \
+		cd $(CWD)/src/$@/; \
+		rubocop cv.rb -A; \
+	}
+
+php-lint:
+	{
+		cd $(CWD)/src/${nonlint_target}/; \	
+		curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar; \	
+		curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar; \	
+		- php phpcs.phar cv.php; \	
+		- php phpcbf.phar cv.php; \	
+	}
+
 
 php:
-	php "$(CWD)/src/$@/cv.php" 
+	{ \
+		cd $(CWD)/src/$@/; \
+		php cv.php; \
+	}	
+
+python-lint:
+	{ \
+		cd $(CWD)/src/${nonlint_target}/; \
+		black cv.py; \
+	} 
 
 python:
-	python3 "$(CWD)/src/$@/cv.py"
+	{ \
+		cd $(CWD)/src/$@/; \
+		python3 cv.py; \
+	}		
 
 bump:
 	sed "s/$(version)/$(next)/" -i version
@@ -62,20 +148,3 @@ version: bump
 push: version
 	git pull --rebase
 	git push -u origin main:main -f
-
-lint: version
-	curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
-	curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
-	npm install --save-dev stylelint stylelint-config-standard
-	curl -o .stylelintrc.json https://raw.githubusercontent.com/dmzoneill/dmzoneill/main/.github/linters/.stylelintrc.json
-	npx standard --fix
-	rubocop src/ruby/cv.rb -A
-	black src/python/cv.py
-	npx stylelint --fix "**/*.css"
-	npx htmlhint "**/*.html"
-	golangci-lint run --fix src/go/cv.go
-	clang-format -i src/cpp/cv.cpp
-	rm .stylelintrc.json
-	- php phpcs.phar src/php/cv.php
-	- php phpcbf.phar src/php/cv.php
-	
---
+++ src/csharp/cv.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using YamlDotNet.RepresentationModel;
+using System.Text.RegularExpressions;
+
+namespace cv
+{
+    class Cv
+    {
+        private YamlMappingNode rootNode;
+        private string html_template;
+        private string yaml_cv;
+        private string html_out_file;
+
+        public Cv()
+        {
+            this.loadValues();
+
+            this.replace_references();
+            this.replace_roles();
+            this.replace_pages();
+
+            foreach (var entry in rootNode.Children)
+            {
+                if (entry.Value.NodeType == YamlNodeType.Sequence)
+                {
+                    var pattern =
+                        @"<"
+                        + ((YamlScalarNode)entry.Key).Value
+                        + ">(.*?)</"
+                        + ((YamlScalarNode)entry.Key).Value
+                        + ">";
+
+                    var m = Regex.Match(
+                        this.html_template,
+                        pattern,
+                        RegexOptions.Multiline | RegexOptions.Singleline
+                    );
+
+                    var replacement = this.replace_sequence(
+                        m.Groups[1].ToString(),
+                        (YamlSequenceNode)entry.Value
+                    );
+
+                    this.html_template = Regex.Replace(
+                        this.html_template,
+                        pattern,
+                        replacement,
+                        RegexOptions.Multiline | RegexOptions.Singleline
+                    );
+                }
+            }
+
+            foreach (var entry in rootNode.Children)
+            {
+                if (entry.Value.NodeType == YamlNodeType.Scalar)
+                {
+                    this.html_template = this.replace_scalar(
+                        this.html_template,
+                        (YamlScalarNode)entry.Key,
+                        (YamlScalarNode)entry.Value
+                    );
+                }
+            }
+
+            File.WriteAllText(this.html_out_file, this.html_template);
+        }
+
+        private void loadValues()
+        {
+            try
+            {
+                string template_file = Environment.GetEnvironmentVariable("template_file");
+                this.html_template = File.ReadAllText(template_file);
+
+                this.html_out_file = Environment.GetEnvironmentVariable("output_file");
+
+                string cv_file = Environment.GetEnvironmentVariable("cv_file");
+                this.yaml_cv = File.ReadAllText(cv_file);
+                var input = new StringReader(this.yaml_cv);
+                var yaml = new YamlStream();
+                yaml.Load(input);
+
+                this.rootNode = (YamlMappingNode)yaml.Documents[0].RootNode;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+        }
+
+        private string replace_scalar(
+            string html,
+            YamlScalarNode key_node,
+            YamlScalarNode value_node
+        )
+        {
+            try
+            {
+                if (html.IndexOf("{" + key_node.Value + "}", StringComparison.Ordinal) > -1)
+                {
+                    html = Regex.Replace(html, "\\{" + key_node.Value + "\\}", value_node.Value);
+                }
+                else if (html.IndexOf("{list_item}", StringComparison.Ordinal) > -1)
+                {
+                    html = html.Replace("{list_item}", value_node.Value);
+                }
+                else
+                {
+                    // not sure
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+
+            return html;
+        }
+
+        private string replace_scalars(string html, YamlMappingNode node)
+        {
+            try
+            {
+                if (html.IndexOf("<children>", StringComparison.Ordinal) > -1 )
+                {
+                    string copy = html;
+                    var pattern = @"<children>(.*?)</children>";
+                    var m = Regex.Match(
+                        copy,
+                        pattern,
+                        RegexOptions.Multiline | RegexOptions.Singleline
+                    );
+
+                    var header = node.Children[0];
+                    var header_title = (YamlScalarNode)header.Value;
+
+                    if (node.Children.Count > 1)
+                    {
+                        var items = node.Children[1];
+                        var subitems = (YamlSequenceNode)items.Value;
+
+                        if (m.Success)
+                        {
+                            var sublist = "";
+
+                            foreach (var childentry in subitems)
+                            {
+                                string replaceitem = m.Groups[1].ToString();
+                                sublist += replaceitem.Replace("{list_item}", ((YamlScalarNode)childentry).ToString());
+                            }
+
+                            copy = Regex.Replace(
+                                copy,
+                                pattern,
+                                sublist,
+                                RegexOptions.Multiline | RegexOptions.Singleline
+                            );
+                        }
+                    }
+                    else
+                    {
+                        copy = Regex.Replace(
+                            copy,
+                            @"<ul.*?</ul>",
+                            "",
+                            RegexOptions.Multiline | RegexOptions.Singleline
+                        );
+                    }
+
+                    copy = copy.Replace("{list_item}", header_title.ToString());
+                    copy = Regex.Replace(
+                        copy,
+                        pattern,
+                        "",
+                        RegexOptions.Multiline | RegexOptions.Singleline
+                    );
+
+                    return copy;
+                }
+
+                if (html.IndexOf("{list_item}", StringComparison.Ordinal) > -1)
+                {
+                    string replacement = "";
+
+                    foreach (var entry in node.Children)
+                    {
+                        string copy = html;
+                        string val = "";
+
+                        if (entry.Value.NodeType == YamlNodeType.Sequence)
+                        {
+                            val = replace_sequence(html, (YamlSequenceNode)entry.Value);
+                        }
+                        else
+                        {
+                            val = ((YamlScalarNode)entry.Value).Value;
+                        }
+
+                        copy = Regex.Replace(copy, "\\{list_item\\}", val, RegexOptions.Multiline | RegexOptions.Singleline);
+                        replacement += copy;
+                    }
+
+                    html = replacement;
+                }
+                else
+                {
+                    foreach (var entry in node.Children)
+                    {
+                        var key = ((YamlScalarNode)entry.Key).Value;
+                        string val = "";
+                        if (entry.Value.NodeType == YamlNodeType.Sequence)
+                        {
+                            val = replace_sequence(html, (YamlSequenceNode)entry.Value);
+                        }
+                        else
+                        {
+                            val = ((YamlScalarNode)entry.Value).Value;
+                        }
+                        html = Regex.Replace(html, "\\{" + key + "\\}", val);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+
+            return html;
+        }
+
+        private string replace_sequence(string html, YamlSequenceNode sequence)
+        {
+            var replacement = "";
+            try
+            {
+                foreach (var X in sequence)
+                {
+                    if (X.NodeType == YamlNodeType.Mapping)
+                    {                        
+                        YamlMappingNode item = (YamlMappingNode)X;
+                        replacement += this.replace_scalars(html, item);
+                    }
+                    else if (X.NodeType == YamlNodeType.Scalar)
+                    {
+                        replacement += this.replace_scalar(
+                            html,
+                            (YamlScalarNode)X,
+                            (YamlScalarNode)X
+                        );
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+            return replacement;
+        }
+
+        private void replace_roles()
+        {
+            try
+            {
+                YamlNode outnode = null;
+                YamlSequenceNode roles = null;
+
+                this.rootNode.Children.TryGetValue("roles", out outnode);
+                roles = (YamlSequenceNode)outnode;
+
+                this.html_template = this.html_template.Replace(
+                    "{current-role}",
+                    ((YamlScalarNode)roles[0]).Value
+                );
+
+                var pattern = @"<previous-roles>(.*?)</previous-roles>";
+                var m = Regex.Match(
+                    this.html_template,
+                    pattern,
+                    RegexOptions.Multiline | RegexOptions.Singleline
+                );
+
+                if (!m.Success)
+                    return;
+
+                roles.Children.RemoveAt(0);
+
+                var replacement = this.replace_sequence(m.Groups[1].ToString(), roles);
+
+                this.html_template = Regex.Replace(
+                    this.html_template,
+                    pattern,
+                    replacement,
+                    RegexOptions.Multiline | RegexOptions.Singleline
+                );
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+        }
+
+        private void replace_pages()
+        {
+            YamlNode outpages = null;
+            YamlSequenceNode pages = null;
+            this.rootNode.Children.TryGetValue("pages", out outpages);
+            pages = (YamlSequenceNode)outpages;
+            var count = 0;
+
+            foreach (var page in pages.Children)
+            {
+                var X = (count + 1).ToString();
+
+                var page_pattern = @"<page" + X + ">(.*?)</page" + X + ">";
+                var m_page = Regex.Match(
+                    this.html_template,
+                    page_pattern,
+                    RegexOptions.Multiline | RegexOptions.Singleline
+                );
+
+                if (!m_page.Success)
+                    return;
+
+                var jobs = (YamlSequenceNode)((YamlMappingNode)page)["jobs"];
+
+                var replacement = "";
+
+                foreach (var job in jobs.Children)
+                {
+                    var job_section = m_page.Groups[1].ToString();
+                    var job_pattern = @"<roles>(.*?)</roles>";
+                    var m_job = Regex.Match(
+                        job_section,
+                        job_pattern,
+                        RegexOptions.Multiline | RegexOptions.Singleline
+                    );
+
+                    var roles_replacement = "";
+                    var roles = (YamlSequenceNode)((YamlMappingNode)job)["roles"];
+
+                    foreach (var role in roles.Children)
+                    {
+                        var role_section = m_job.Groups[1].ToString();
+                        var role_pattern = @"<details>(.*?)</details>";
+                        var m_role = Regex.Match(
+                            role_section,
+                            role_pattern,
+                            RegexOptions.Multiline | RegexOptions.Singleline
+                        );
+                        var details = (YamlSequenceNode)((YamlMappingNode)role)["details"];
+
+                        var details_replacement = "";
+                        foreach (var detail in details.Children)
+                        {
+                            var detail_copy = this.replace_scalar(
+                                m_role.Groups[1].ToString(),
+                                (YamlScalarNode)detail,
+                                (YamlScalarNode)detail
+                            );
+                            details_replacement += detail_copy;
+                        }
+
+                        role_section = Regex.Replace(
+                            role_section,
+                            role_pattern,
+                            details_replacement,
+                            RegexOptions.Multiline | RegexOptions.Singleline
+                        );
+
+                        role_section = this.replace_scalars(role_section, (YamlMappingNode)role);
+
+                        roles_replacement += role_section;
+                    }
+
+                    job_section = Regex.Replace(
+                        job_section,
+                        job_pattern,
+                        roles_replacement,
+                        RegexOptions.Multiline | RegexOptions.Singleline
+                    );
+
+                    job_section = this.replace_scalars(job_section, (YamlMappingNode)job);
+
+                    replacement += job_section;
+                }
+
+                this.html_template = Regex.Replace(
+                    this.html_template,
+                    page_pattern,
+                    replacement,
+                    RegexOptions.Multiline | RegexOptions.Singleline
+                );
+
+                count += 1;
+            }
+        }
+
+        private void replace_references()
+        {
+            try
+            {
+                var pattern = @"<references>(.*?)</references>";
+                var m = Regex.Match(
+                    this.html_template,
+                    pattern,
+                    RegexOptions.Multiline | RegexOptions.Singleline
+                );
+                YamlNode outnode = null;
+                YamlSequenceNode references = null;
+
+                if (!m.Success)
+                    return;
+
+                this.rootNode.Children.TryGetValue("references", out outnode);
+                references = (YamlSequenceNode)outnode;
+
+                var replacement = this.replace_sequence(m.Groups[1].ToString(), references);
+                this.html_template = Regex.Replace(
+                    this.html_template,
+                    pattern,
+                    replacement,
+                    RegexOptions.Multiline | RegexOptions.Singleline
+                );
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var x = new Cv();
+        }
+    }
+}
